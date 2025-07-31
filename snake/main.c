cc4896d6537a6c4d4474718823e5be6d68a8d6ac
Alex <110344381+axgw@users.noreply.github.com>
2025-07-31 23:49:28 +0200

    Reorganizing code

--- snake/main.c
+++ snake/main.c
@@ -19,60 +19,75 @@ typedef struct {
   int y;
 } vec2_t;
 
+// Render constants
 static const int SCREEN_WIDTH	= 801;
 static const int SCREEN_HEIGHT = 800;
+static const uint16_t GRID_SIZE = 25;
 
 static const SDL_Color COLOR_BG_BLACK    = {0x00, 0x00, 0x00, 0x00};
 static const SDL_Color COLOR_BG_GREY     = {0x10, 0x10, 0x10, 0x00};
-
-
 static const SDL_Color COLOR_PLAYER_HEAD = {0x00, 0x00, 0xFF, 0x00};
 static const SDL_Color COLOR_PLAYER_TAIL = {0x00, 0x00, 0xFF, 0x00};
 static const SDL_Color COLOR_FRUIT       = {0xFF, 0x00, 0x00, 0x00};
 
-static const uint16_t GRID_SIZE = 25;
+static struct {
+  SDL_Window* window;
+  SDL_Surface* surface;
+  SDL_Event event;
+  bool quit_window;
+  uint32_t frame_count;
+} render = {
+  .window = NULL,
+  .surface = NULL,
+  .quit_window = false,
+  .frame_count = 0
+};
 
-// static variables
-static int frame_count;
+//  struct {
+//   SDL_Window* window;
+//   SDL_Surface* surface;
+//   SDL_Event;
+//   bool quit_window;
+// } render_t;
 
-static SDL_Window* window = NULL;
-static SDL_Surface* surface= NULL;
-static SDL_Event e;
+// render_t render = {
+//   .window = NULL,
+//   .surface = NULL,
+//   .quit_window = false
+// };
 
-static bool quit_window = false;
+// Render functions
+int render_init(void);
+void render_handle_events(void);
+void render_background(void);
+void render_quit(void);
 
-direction_e last_dir = DIR_RIGHT;
 
+
+// Game 
 static const vec2_t HEAD_COORD_INIT = {4 * GRID_SIZE, 0};
-
 static vec2_t head_coord;
 static vec2_t last_head_coord;
-/* vec2_t tail_coords[32 * 32 - 1] = {0}; */
-
+static vec2_t fruit_coord;
 static vec2_t tail_coords[32 * 32 - 1];
-
-static uint16_t delay_duration_ms = 400;
-
-
 static size_t tail_count = 4;
+static uint16_t delay_duration_ms = 400;
+direction_e last_dir = DIR_RIGHT;
 
+
+// Game functions
+void game_on_init(void);
 void tail_init();
 void tail_update();
 void tail_draw();
-
-void quit_sdl();
-void draw_background();
+void fruit_draw(void);
+void fruit_generate(void);
 void update_input(vec2_t *rect);
 
-// ...x
-// [4, 0]
-// ---x
-// ..........x
-//        ---x
-//
 
-void on_game_init(void) {
+void game_on_init(void) {
   head_coord = HEAD_COORD_INIT;
+  fruit_generate();
   // tail_coords[0] = (vec2_t){HEAD_COORD_INIT.x + 1 * GRID_SIZE, HEAD_COORD_INIT.y};
   // tail_coords[1] = (vec2_t){HEAD_COORD_INIT.x - 0 * GRID_SIZE, HEAD_COORD_INIT.y};
   // tail_coords[2] = (vec2_t){HEAD_COORD_INIT.x - 1 * GRID_SIZE, HEAD_COORD_INIT.y};
@@ -81,24 +96,19 @@ void on_game_init(void) {
 void tail_draw() {
   for (size_t i = 0; i < tail_count; i++) {
     SDL_FillRect(
-      surface,
+      render.surface,
       &(SDL_Rect){tail_coords[i].x, tail_coords[i].y, GRID_SIZE, GRID_SIZE},
-      SDL_MapRGB(surface->format, COLOR_PLAYER_TAIL.r, COLOR_PLAYER_TAIL.g, COLOR_PLAYER_TAIL.b)
+      SDL_MapRGB(render.surface->format, COLOR_PLAYER_TAIL.r, COLOR_PLAYER_TAIL.g, COLOR_PLAYER_TAIL.b)
     );
   }
 }
 
-// ---x
-// [0,0] [1,0] [2,0] 
-
-//       [  ] [2,1]
-//
-// 
-
-void shift_right(vec2_t* start, vec2_t* end, const size_t dif)
-{
-  for (size_t i = (end - start) - 1 ; i > dif - 1 ; i--)
-    start[i] = start[i - dif];
+void fruit_draw(void) {
+  SDL_FillRect(
+    render.surface,
+    &(SDL_Rect){fruit_coord.x, fruit_coord.y, GRID_SIZE, GRID_SIZE},
+    SDL_MapRGB(render.surface->format, COLOR_FRUIT.r, COLOR_FRUIT.g, COLOR_FRUIT.b)
+  );
 }
 
 void tail_update() {
@@ -114,90 +124,103 @@ void tail_update() {
   }
 }
 
+// ----------------------------------------------------------------------------
+// render
+// ----------------------------------------------------------------------------
 
-int main(void) {
-  on_game_init();
-	if (SDL_Init( SDL_INIT_VIDEO ) < 0)	{
+int render_init(void) {
+  if (SDL_Init( SDL_INIT_VIDEO ) < 0)	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 	}
-  window = SDL_CreateWindow(
-    "SNAKE WEbos",
+  render.window = SDL_CreateWindow(
+    "SNAKE webOS",
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     SDL_WINDOW_SHOWN
   );
-  if ( window == NULL )	{
+  if ( render.window == NULL )	{
     printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
   //	Get window surface
-  surface = SDL_GetWindowSurface(window);
+  render.surface = SDL_GetWindowSurface(render.window);
+}
 
-
-  while (!quit_window) {
-
-    printf("%i\n", frame_count++);
-    
-    while (SDL_PollEvent(&e) != 0)	{
-      switch(e.type) {
-        case SDL_KEYDOWN:
-          switch(e.key.keysym.sym) {
-            case SDLK_UP:
-              last_dir = DIR_UP;
-              break;
-            case SDLK_DOWN:
-              last_dir = DIR_DOWN;
-              break;
-            case SDLK_LEFT:
-              last_dir = DIR_LEFT;
-              break;
-            case SDLK_RIGHT:
-              last_dir = DIR_RIGHT;
-              break;
-          }
-          break;
-        case SDL_QUIT:
-          quit_window = true;
-          break;
-      }
+void render_handle_events(void) {
+  while (SDL_PollEvent(&render.event) != 0)	{
+    switch(render.event.type) {
+      case SDL_KEYDOWN:
+        switch(render.event.key.keysym.sym) {
+          case SDLK_UP:
+            last_dir = DIR_UP;
+            break;
+          case SDLK_DOWN:
+            last_dir = DIR_DOWN;
+            break;
+          case SDLK_LEFT:
+            last_dir = DIR_LEFT;
+            break;
+          case SDLK_RIGHT:
+            last_dir = DIR_RIGHT;
+            break;
+        }
+        break;
+      case SDL_QUIT:
+        render.quit_window = true;
+        break;
     }
+  }
+};
+
+int main(void) {
+	int ec = render_init();
+  if (ec != 0) return ec;
+  
+  game_on_init();
+
+  while (!render.quit_window) {
+
+    printf("%i\n", render.frame_count++);
+    
+    render_handle_events();
 
     last_head_coord = head_coord;
     update_input(&head_coord);
     tail_update();
     
-    draw_background();
+    render_background();
 
     SDL_Rect player_head = {head_coord.x, head_coord.y, 25, 25};
 
     tail_draw();
+    fruit_draw();
     SDL_FillRect(
-      surface,
+      render.surface,
       &player_head,
-      SDL_MapRGB(surface->format, COLOR_FRUIT.r, COLOR_FRUIT.g, COLOR_FRUIT.b)
+      SDL_MapRGB(render.surface->format, COLOR_FRUIT.r, COLOR_FRUIT.g, COLOR_FRUIT.b)
     );
 
-    SDL_UpdateWindowSurface( window );
+    SDL_UpdateWindowSurface( render.window );
     SDL_Delay(delay_duration_ms);
     // delay_duration_ms -= 20;
   }
 
   /// end
 
-  quit_sdl();
+  render_quit();
 	return EXIT_SUCCESS;
 }
 
 /// ---------------------------------------------------------------------------
 
-void quit_sdl(void) {
-  SDL_DestroyWindow( window );
+void render_quit(void) {
+  SDL_DestroyWindow( render.window );
   SDL_Quit();
 }
 
-void draw_background() {
+void render_background(void) {
   // SDL_Rect bg_rect = {0, 0, GRID_SIZE, GRID_SIZE};
   // for (size_t i = 0; i < 32; i++) {
   //   for (size_t j = 0; j < 32; j++) {
@@ -212,10 +235,11 @@ void draw_background() {
   //   bg_rect.y += GRID_SIZE;
   //   bg_rect.x = (i % 2 == 0) ? GRID_SIZE : 0;
   // }
+
   SDL_FillRect(
-      surface,
+      render.surface,
       NULL,
-      SDL_MapRGB(surface->format, COLOR_BG_BLACK.r, COLOR_BG_BLACK.g, COLOR_BG_BLACK.b)
+      SDL_MapRGB(render.surface->format, COLOR_BG_BLACK.r, COLOR_BG_BLACK.g, COLOR_BG_BLACK.b)
     );
 }
 
@@ -234,4 +258,8 @@ void update_input(vec2_t *rect) {
       rect -> x += GRID_SIZE;
       break;
   }
+}
+
+void fruit_generate(void) {
+  fruit_coord = (vec2_t){0,0};
 }
